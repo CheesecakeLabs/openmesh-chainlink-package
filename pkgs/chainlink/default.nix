3a3b55ffd044defc154eabc608058c1a8eb214f7
Bruno Nascimento <brunonascimentodev@gmail.com>
2024-11-14 16:32:34 -0300

    add default chainlink package

--- pkgs/chainlink/default.nix
+++ pkgs/chainlink/default.nix
@@ -34,21 +34,48 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  buildGoModule,
 }:
 
-stdenv.mkDerivation (finalAttrs: {
+buildGoModule rec {
   pname = "chainlink";
   version = "2.18.0";
 
   src = fetchFromGitHub {
     owner = "smartcontractkit";
-    repo = finalAttrs.pname;
-    rev = "v${finalAttrs.version}";
+    repo = pname;
+    rev = "v${version}";
     sha256 = "sha256-ifu+5fzujIKsZQiOA+3bsh5L34dYfVFG6Nk3p+N5kO4=";
-    fetchSubmodules = true;
   };
 
-  nativeBuildInputs =
+  vendorHash = "sha256-s98pfExSofXZMq2l+ctGgab4gUQ87hUZUZX43PCWLP8=";
+  proxyVendor = true;
+  dontFixup = true;
+
+  ldflags = [
+    "-X github.com/smartcontractkit/chainlink/v2/core/static.Version=2.18.0"
+    "-X github.com/smartcontractkit/chainlink/v2/core/static.Sha=fb7d6e88ea3471909a4f9aa29992ec080bba9057"
+  ];
+
+  buildPhase = ''
+    go build -ldflags "${lib.concatStringsSep " " ldflags}" .
+
+    ls -la
+
+    # Copy the binary to the output directory
+    mkdir -p "$out/bin"
+    cp chainlink "$out/bin/chainlink"
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    go install -ldflags "${lib.concatStringsSep " " ldflags}" . 
+
+    runHook postInstall  
+  '';
+
+    nativeBuildInputs =
     [
       go
       postgresql_16
@@ -89,44 +116,6 @@ stdenv.mkDerivation (finalAttrs: {
     IOKit
   ];
 
-  outputs = [ "out" ];
-
-  # Set up environment and build flags
-  preBuild = ''
-    # Override $HOME to be writable
-    export HOME=$(pwd)
-
-    echo "Setting NPM strict-ssl to false for this build..."
-    npm config set strict-ssl false
-    npm config rm proxy 
-    npm config rm https-proxy
-  '';
-
-  # Installation phase to install the Chainlink binary
-  installPhase = ''
-    # run sed to replace GOFLAG lines
-    sed -i "" 's/GO_LDFLAGS := $(shell tools\/bin\/ldflags)//g' GNUmakefile && sed -i "" 's/\$(GO_LDFLAGS)/-X github.com\/smartcontractkit\/chainlink\/v2\/core\/static.Version=2.18.0 -X github.com\/smartcontractkit\/chainlink\/v2\/core\/static.Sha=0e855379b9f4ff54944f8ee9918b7bbfc0a67469/g' GNUmakefile
-
-    make install
-
-    # Copy the binary to the output directory
-    mkdir -p "$out/bin"
-    cp chainlink "$out/bin/chainlink"
-
-    # get the correct libwasmvm name for the platform
-    if [ "$(uname)" == "Darwin" ]; then
-      libwasmvm="libwasmvm.dylib"
-    else
-      libwasmvm="libwasmvm.so"
-    fi
-
-    # Fix the install_name of the wasmvm dylib
-    install_name_tool -id "@rpath/$libwasmvm" "$out/lib/$libwasmvm"
-  '';
-
-  dontFixup = true;
-
-  # Metadata for the package
   meta = with lib; {
     description = "Chainlink is a decentralized oracle network.";
     homepage = "https://github.com/smartcontractkit/chainlink";
@@ -134,4 +123,4 @@ stdenv.mkDerivation (finalAttrs: {
     maintainers = [ "brunonascdev" ];
     platforms = platforms.unix;
   };
-})
+}
